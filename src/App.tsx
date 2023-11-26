847ce8b54c5782c50bbbd4d09b52724e2f860302
vladislav@book <vladovinkin@gmail.com>
2023-11-26 22:42:40 +0300

    - сохранил код предыдущего урока - очистил App.tsx для следующего

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,5 @@
-import { useEffect, useState } from 'react'
-
 const App = () => {
-  const [type, setType] = useState('users')
-  const [data, setData] = useState([])
-  const [pos, setPos] = useState({
-    x: 0,
-    y: 0,
-  })
-
-  // useEffect(() => console.log('useEffect render'))
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then((response) => response.json())
-      .then((json) => setData(json))
-
-    return () => {
-      console.log(`clean type ${type}`)
-    }
-  }, [type])
-
-  const mouseMoveHandler = (event: MouseEvent) => {
-    setPos({
-      x: event.clientX,
-      y: event.clientY,
-    })
-  }
-
-  useEffect(() => {
-    console.log('Component did mount')
-    window.addEventListener('mousemove', mouseMoveHandler)
-
-    return () => window.removeEventListener('mousemove', mouseMoveHandler)
-  }, [])
-
-  return (
-    <div>
-      <h1>Ресурс: {type}</h1>
-      <button onClick={() => setType('users')}>Пользователи</button>
-      <button onClick={() => setType('todos')}>Todo</button>
-      <button onClick={() => setType('posts')}>Посты</button>
-
-      {/*<pre>{JSON.stringify(data, null, 2)}</pre>*/}
-      <pre>{JSON.stringify(pos, null, 2)}</pre>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
