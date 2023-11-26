0d6d2373bcb4b16542bc8cd8666afdbc0c0f0d02
vladislav@book <vladovinkin@gmail.com>
2023-11-26 23:38:02 +0300

    - сохранил код предыдущего урока - очистил App.tsx для следующего

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-import { useEffect, useRef, useState } from 'react'
-
 const App = () => {
-  const [value, setValue] = useState('initial')
-  const renderCount = useRef(1)
-  const inputRef = useRef<HTMLInputElement | null>(null)
-  const prevValue = useRef('')
-
-  useEffect(() => {
-    renderCount.current++
-    console.log(inputRef.current!.value)
-  })
-
-  useEffect(() => {
-    prevValue.current = value
-  }, [value])
-
-  const focus = () => inputRef.current!.focus()
-
-  return (
-    <div>
-      <h1>Количество рендеров: {renderCount.current}</h1>
-      <h2>Прошлое состояние: {prevValue.current}</h2>
-      <input
-        ref={inputRef}
-        type="text"
-        onChange={(event) => setValue(event.target.value)}
-        value={value}
-      />
-      <button className="btn btn-success" onClick={focus}>
-        Фокус
-      </button>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
