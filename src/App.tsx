c0d08825976f0ddccb470d5f2e9ca251f3ea16db
vladislav@book <vladovinkin@gmail.com>
2023-11-26 23:08:02 +0300

    нормальный подсчёт рендеров при помощи внешней переменной

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from 'react'
 
+let renderCount = 1
+
 const App = () => {
-  const [renderCount, setRenderCount] = useState(1)
+  // const [renderCount, setRenderCount] = useState(1)
+  const [value, setValue] = useState('initial')
 
   useEffect(() => {
-    setRenderCount((prevRenderCount) => prevRenderCount + 1)
+    // setRenderCount((prevRenderCount) => prevRenderCount + 1)
+    renderCount++
   })
 
   return (
     <div>
       <h1>Количество рендеров: {renderCount}</h1>
+      <input
+        type="text"
+        onChange={(event) => setValue(event.target.value)}
+        value={value}
+      />
     </div>
   )
 }
