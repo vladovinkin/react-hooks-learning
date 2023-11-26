e01ba8340575ec52e42a9ae812077064b4997226
vladislav@book <vladovinkin@gmail.com>
2023-11-26 23:03:19 +0300

    задача "посчитать количество рендеров" привела к бесконечному циклу

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useEffect, useState } from 'react'
+
 const App = () => {
-  return <div></div>
+  const [renderCount, setRenderCount] = useState(1)
+
+  useEffect(() => {
+    setRenderCount((prevRenderCount) => prevRenderCount + 1)
+  })
+
+  return (
+    <div>
+      <h1>Количество рендеров: {renderCount}</h1>
+    </div>
+  )
 }
 
 export default App
