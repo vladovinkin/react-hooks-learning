48411f10ba8391fa2b75b60fdc9431c014ff2351
vladislav@book <vladovinkin@gmail.com>
2023-11-29 11:33:51 +0300

    подготовил шаблон под useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
+import { useState } from 'react'
+
 const App = () => {
-  return <div></div>
+  const [colored, setColored] = useState(false)
+  const [count, setCount] = useState(1)
+
+  const styles = {
+    color: colored ? 'darkred' : 'black',
+  }
+
+  return (
+    <>
+      <h1 style={styles}>Количество элементов: </h1>
+      <button
+        className="btn btn-success me-3"
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Добавить
+      </button>
+      <button
+        className="btn btn-warning"
+        onClick={() => setColored((prev) => !prev)}
+      >
+        Изменить
+      </button>
+    </>
+  )
 }
 
 export default App
