4fe372ac6e39d238aae6da53d0983dac2a80418a
vladislav@book <vladovinkin@gmail.com>
2023-11-29 13:15:24 +0300

    подготовил файлы для следующего занятия

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,7 @@
-import { useCallback, useState } from 'react'
-import ItemsList from './ItemsList'
-
 const App = () => {
-  const [colored, setColored] = useState(false)
-  const [count, setCount] = useState(1)
-
-  const styles = {
-    color: colored ? 'darkred' : 'black',
-  }
-
-  const generateItemsFromAPI = useCallback(
-    (startNumber: number) => {
-      return new Array(count)
-        .fill('')
-        .map((_, i) => `Элемент ${i + startNumber}`)
-    },
-    [count],
-  )
-
   return (
     <>
-      <h1 style={styles}>Количество элементов: {count}</h1>
-      <button
-        className="btn btn-success me-3"
-        onClick={() => setCount((prev) => prev + 1)}
-      >
-        Добавить
-      </button>
-      <button
-        className="btn btn-warning"
-        onClick={() => setColored((prev) => !prev)}
-      >
-        Изменить
-      </button>
-      <ItemsList getItems={generateItemsFromAPI} />
+
     </>
   )
 }
